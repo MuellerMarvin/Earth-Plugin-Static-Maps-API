f5f12e76e85cfc277746b481ef59b1ce42704505
Marvin Müller <marvin.mueller@code.berlin>
2023-07-01 18:19:58 +0200

    Generate basic map with maplibre

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log("Hello World!");
+var fs = require('fs');
+var path = require('path');
+var mbgl = require('@maplibre/maplibre-gl-native');
+var sharp = require('sharp');
+var axios = require('axios');
+
+var options = {
+    request: function(req: any, callback: (error: any, response: any) => void) {
+      // Log the request URL
+      console.log('Request URL:', req.url);
+  
+      axios.get(req.url, { responseType: 'arraybuffer' })
+        .then(function(response: any) {
+          // Forward the response to the maplibre-gl-native
+          callback(null, { data: new Uint8Array(response.data) });
+        })
+        .catch(function(error: any) {
+          // Handle errors
+          callback("Error on: "+ req.url + "\n" + error, null);
+        });
+    },
+    ratio: 1
+};
+
+var map = new mbgl.Map(options);
+
+map.load(require('./styles/style.json'));
+
+map.render({zoom: 0}, function(err: any, buffer: any) {
+    if (err) throw err;
+
+    map.release();
+
+    var image = sharp(buffer, {
+        raw: {
+            width: 512,
+            height: 512,
+            channels: 4
+        }
+    });
+
+    // Convert raw image buffer to PNG
+    image.toFile('image.png', function(err: any) {
+        if (err) throw err;
+    });
+});
