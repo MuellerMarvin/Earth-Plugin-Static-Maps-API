77435a0d552e99b0a4748cd9657f726632f4e973
Marvin Müller <marvin.mueller@code.berlin>
2023-08-20 20:13:14 +0200

    Remove outdated mapimage tests

--- tests/maps.test.ts
+++ tests/maps.test.ts
@@ -2,20 +2,4 @@ import { getMapImage } from "../src/maps";
 import { Coordinates } from "../src/types";
 
 describe("getMapImage", () => {
-    it("should return a buffer", async () => {
-        const coordinates: Coordinates = {
-        lat: 13.405,
-        lon: 52.52,
-        };
-        const buffer: Buffer = await getMapImage(coordinates);
-        expect(buffer).toBeInstanceOf(Buffer);
-    });
-    it("should return a buffer with a length greater than 0", async () => {
-        const coordinates: Coordinates = {
-        lat: 0,
-        lon: 0,
-        };
-        const buffer: Buffer = await getMapImage(coordinates);
-        expect(buffer.length).toBeGreaterThan(0);
-    });
 });
