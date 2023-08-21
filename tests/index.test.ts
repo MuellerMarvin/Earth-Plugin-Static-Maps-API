bd987372936e5e01d4a33d6e0df17fbf41eb1ec4
Marvin Müller <marvin.mueller@code.berlin>
2023-08-21 23:01:14 +0200

    Increase test timeout for the default get function

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,5 +5,5 @@ describe('GET /', () => {
   it('should return 200 OK', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-  });
+  }, 10000); // 10 second timeout as image generation takes a while
 });
