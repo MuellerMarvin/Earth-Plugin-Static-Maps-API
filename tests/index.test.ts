2c528f4b557f6ca8737d5eefdd4d66e6975d3882
Marvin Müller <marvin.mueller@code.berlin>
2023-08-26 21:49:55 +0200

    Remove index tests temporarily

--- tests/index.test.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest';
-import { app } from '../src/index';
-
-describe('GET /', () => {
-  it('should return 200 OK', async () => {
-    const response = await request(app).get('/');
-    expect(response.status).toBe(200);
-  }, 10000); // 10 second timeout as image generation takes a while
-});
